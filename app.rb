d4118e7b3d64afbf1010fbe8f8c05a605f88e89b
Asilver-jpg <asilver7493@gmail.com>
2020-03-31 11:34:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+get '/newteam' do
+    erb :newteam
+end
+post '/team' do
+paramaters= params
 
+@name= paramaters[:name]
+@point= paramaters[:pg]
+@coach= paramaters[:coach]
+@shoot= paramaters[:sg]
+@small= paramaters[:sf]
+@power= paramaters[:pf]
+@center= paramaters[:c]
+erb :team
+end
 end
